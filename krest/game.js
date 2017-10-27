926b3ab38250a99c1cdb9f9a0f7751419edf73f2
ShalunBdk <shalunbdk@gmail.com>
2017-10-27 16:20:55 +0700

    Add files via upload

--- krest/game.js
+++ krest/game.js
@@ -516,7 +516,7 @@ game.newLoopFromConstructor('myGame', function () {
   };
 
   this.entry = function () { // [optional]
-  /*if(user.loaded == false){
+  if(user.loaded == false){
 	VK.api("users.get", {'fields':'photo_50'}, function(data) {
 			user.name = '' + data.response[0].first_name;
 			user.id = '' + data.response[0].id;
@@ -576,7 +576,7 @@ game.newLoopFromConstructor('myGame', function () {
 			photo2.setImage(PHOTO_COIN);
 		});
 	user.loaded = true;
-  }*/
+  }
     OOP.clearArr(podarki);
     score = 0;
 	if((pjs.resources.isLoaded() == true) && user.loaded)GAME = 0;
